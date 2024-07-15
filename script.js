03074201ba4adca467ff15abcad2e5afdd87fa1a
shahzeb-alam <166326536+shahzeb-alam@users.noreply.github.com>
2024-07-15 12:20:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -91,6 +91,13 @@ function typewriter()
   setTimeout("typewriter()", iSpeed);
  }
 }
-
-
 typewriter();
+
+//PPH Script
+(function(d, s) {
+    var useSSL = 'https:' == document.location.protocol;
+    var js, where = d.getElementsByTagName(s)[0],
+    js = d.createElement(s);
+    js.src = (useSSL ? 'https:' : 'http:') +  '//www.peopleperhour.com/hire/4113407163/11511552.js?width=300&height=135&orientation=vertical&theme=dark&rnd='+parseInt(Math.random()*10000, 10);
+    try { where.parentNode.insertBefore(js, where); } catch (e) { if (typeof console !== 'undefined' && console.log && e.stack) { console.log(e.stack); } }
+}(document, 'script'));
